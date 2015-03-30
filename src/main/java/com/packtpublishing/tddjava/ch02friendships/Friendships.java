095827be6d979e934add817280848950b7d01ffb
Viktor Farcic <viktor@farcic.com>
2015-03-30 23:07:32 +0200

    Modified JUnit example

--- src/main/java/com/packtpublishing/tddjava/ch02friendships/Friendships.java
+++ src/main/java/com/packtpublishing/tddjava/ch02friendships/Friendships.java
@@ -11,17 +11,24 @@ public class Friendships {
     }
 
     public List<String> getFriendsList(String person) {
-        if (!friendships.containsKey(person)) return Collections.emptyList();
+        if (!friendships.containsKey(person)) {
+            return Collections.emptyList();
+        }
         return friendships.get(person);
     }
     
     public boolean areFriends(String person1, String person2) {
-        return friendships.containsKey(person1) && friendships.get(person1).contains(person2);
+        return friendships.containsKey(person1)
+                && friendships.get(person1).contains(person2);
     }
 
     private void addFriend(String person, String friend) {
-        if (!friendships.containsKey(person)) friendships.put(person, new ArrayList<String>());
+        if (!friendships.containsKey(person)) {
+            friendships.put(person, new ArrayList<String>());
+        }
         List<String> friends = friendships.get(person);
-        if (!friends.contains(friend)) friends.add(friend);
+        if (!friends.contains(friend)) {
+            friends.add(friend);
+        }
     }
 }
--- src/test/java/com/packtpublishing/tddjava/ch02friendships/FriendshipsTest.java
+++ src/test/java/com/packtpublishing/tddjava/ch02friendships/FriendshipsTest.java
@@ -6,19 +6,28 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FriendshipsTest {
+
+    Friendships friendships;
+
     @BeforeClass
     public static void beforeClass() {
         // This method will be executed once on initialization time
     }
 
+    @Before
+    public void before() {
+        friendships = new Friendships();
+        friendships.makeFriends("Joe", "Audrey");
+        friendships.makeFriends("Joe", "Peter");
+        friendships.makeFriends("Joe", "Michael");
+        friendships.makeFriends("Joe", "Britney");
+        friendships.makeFriends("Joe", "Paul");
+    }
+
+
     @AfterClass
     public static void afterClass() {
         // This method will be executed once when all test are executed
-    }
-
-    @Before
-    public void before() {
-        // This method will be executed once before each test execution
     }
 
     @After
@@ -27,41 +36,19 @@ public class FriendshipsTest {
     }
 
     @Test
-    public void test1() {
-        Friendships friendships = new Friendships();
-
-        Assert.assertTrue("Joe does not have friends", friendships.getFriendsList("Joe").isEmpty());
+    public void alexDoesNotHaveFriends() {
+        Assert.assertTrue("Alex does not have friends", friendships.getFriendsList("Alex").isEmpty());
     }
 
     @Test
-    public void test2() {
-        Friendships friendships = new Friendships();
-        friendships.makeFriends("Joe", "Audrey");
-
-        Assert.assertTrue("Joe and Audrey are friends", friendships.areFriends("Joe", "Audrey"));
-        Assert.assertTrue("Audrey and Joe are friends", friendships.areFriends("Audrey", "Joe"));
-
-        Assert.assertTrue("Joe is Audrey's friend", friendships.getFriendsList("Joe").contains("Audrey"));
-        Assert.assertEquals("Joe has 1 friend", 1, friendships.getFriendsList("Joe").size());
-
-        Assert.assertTrue("Audrey is Joe's friend", friendships.getFriendsList("Audrey").contains("Joe"));
-        Assert.assertEquals("Joe has 1 friend", 1, friendships.getFriendsList("Audrey").size());
+    public void joeHas5Friends() {
+        Assert.assertEquals("Joe has 5 friends", 5, friendships.getFriendsList("Joe").size());
     }
 
-    @Test(timeout = 10)
-    public void test3() {
-        Friendships friendships = new Friendships();
-        friendships.makeFriends("Joe", "Audrey");
-        friendships.makeFriends("Joe", "Peter");
-        friendships.makeFriends("Joe", "Michael");
-        friendships.makeFriends("Joe", "Britney");
-        friendships.makeFriends("Joe", "Paul");
-
-        Assert.assertEquals("Joe has 5 friends", 5, friendships.getFriendsList("Joe").size());
-        Assert.assertTrue("Joe and Paul are friends", friendships.areFriends("Joe", "Paul"));
-        Assert.assertFalse("Joe and Ralph are not friends", friendships.areFriends("Joe", "Ralph"));
-
+    @Test
+    public void joeIsFriendWithEveryone() {
         List<String> friendsOfJoe = Arrays.asList("Audrey", "Peter", "Michael", "Britney", "Paul");
         Assert.assertTrue(friendships.getFriendsList("Joe").containsAll(friendsOfJoe));
     }
+
 }
