cb50cbf51e9dee18b6e18a2ca22d413b52f540f2
Viktor Farcic <viktor@farcic.com>
2015-04-01 23:21:36 +0200

    Modified EasyMock and Mockito examples

--- src/test/java/com/packtpublishing/tddjava/ch02friendships/FriendshipsMongoAssertJTest.java
+++ src/test/java/com/packtpublishing/tddjava/ch02friendships/FriendshipsMongoAssertJTest.java
@@ -6,11 +6,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FriendshipsMongoAssertJTest {
@@ -22,49 +23,28 @@ public class FriendshipsMongoAssertJTest {
     FriendsCollection friends;
 
     @Test
-    public void test1() {
-        doReturn(new Person("Joe")).when(friends).findByName(anyString());
-
-        assertThat(friendships.getFriendsList("Joe")).isEmpty();
+    public void mockingWorksAsExpected() {
+        Person joe = new Person("Joe");
+        doReturn(joe).when(friends).findByName("Joe");
+        assertThat(friends.findByName("Joe")).isEqualTo(joe);
     }
 
     @Test
-    public void test2() {
-        Person joe = new Person("Joe");
-        doReturn(joe).when(friends).findByName("Joe");
-
-        Person audrey = new Person("Audrey");
-        doReturn(audrey).when(friends).findByName("Audrey");
-
-        doNothing().when(friends).save(any(Person.class));
-
-
-        friendships.makeFriends("Joe", "Audrey");
-
-        assertThat(friendships.areFriends("Joe", "Audrey")).isTrue();
-        assertThat(friendships.areFriends("Audrey", "Joe")).isTrue();
-
-        assertThat(friendships.getFriendsList("Joe")).hasSize(1).contains("Audrey");
-
-        assertThat(friendships.getFriendsList("Audrey")).hasSize(1).contains("Joe");
+    public void alexDoesNotHaveFriends() {
+        assertThat(friendships.getFriendsList("Alex")).isEmpty();
     }
 
-    @Test(timeout = 500)
-    public void test3() {
-        Person joe = new Person("Joe");
+    @Test
+    public void joeHas5Friends() {
+        List<String> expected = Arrays.asList(
+                new String[]{"Audrey", "Peter", "Michael", "Britney", "Paul"}
+        );
+        Person joe = spy(new Person("Joe"));
         doReturn(joe).when(friends).findByName("Joe");
-
-        doNothing().when(friends).save(any(Person.class));
-
-        friendships.makeFriends("Joe", "Audrey");
-        friendships.makeFriends("Joe", "Peter");
-        friendships.makeFriends("Joe", "Michael");
-        friendships.makeFriends("Joe", "Britney");
-        friendships.makeFriends("Joe", "Paul");
-
-        assertThat(friendships.areFriends("Joe", "Paul")).isTrue();
-        assertThat(friendships.areFriends("Audrey", "Ralph")).isFalse();
-
-        assertThat(friendships.getFriendsList("Joe")).containsOnly("Audrey", "Peter", "Michael", "Britney", "Paul");
+        doReturn(expected).when(joe).getFriends();
+        assertThat(friendships.getFriendsList("Joe"))
+                .hasSize(5)
+                .containsOnly("Audrey", "Peter", "Michael", "Britney", "Paul");
     }
+
 }
--- src/test/java/com/packtpublishing/tddjava/ch02friendships/FriendshipsMongoEasyMockTest.java
+++ src/test/java/com/packtpublishing/tddjava/ch02friendships/FriendshipsMongoEasyMockTest.java
@@ -1,17 +1,18 @@
 package com.packtpublishing.tddjava.ch02friendships;
 
-import org.easymock.EasyMockRunner;
-import org.easymock.Mock;
-import org.easymock.MockType;
-import org.easymock.TestSubject;
+import org.easymock.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.easymock.EasyMock.*;
 
 @RunWith(EasyMockRunner.class)
 public class FriendshipsMongoEasyMockTest {
+
     @TestSubject
     FriendshipsMongo friendships = new FriendshipsMongo();
 
@@ -19,57 +20,33 @@ public class FriendshipsMongoEasyMockTest {
     FriendsCollection friends;
 
     @Test
-    public void test1() {
-        expect(friends.findByName(anyString())).andReturn(new Person("Joe"));
-
+    public void mockingWorksAsExpected() {
+        Person joe = new Person("Joe");
+        expect(friends.findByName("Joe")).andReturn(joe);
         replay(friends);
-
-        assertThat(friendships.getFriendsList("Joe")).isEmpty();
-
-        verify(friends);
+        assertThat(friends.findByName("Joe")).isEqualTo(joe);
     }
 
     @Test
-    public void test2() {
-        Person joe = new Person("Joe");
-        expect(friends.findByName("Joe")).andReturn(joe).anyTimes();
-
-        Person audrey = new Person("Audrey");
-        expect(friends.findByName("Audrey")).andReturn(audrey).anyTimes();
-
-        replay(friends);
-
-        friendships.makeFriends("Joe", "Audrey");
-
-        verify(friends);
-
-        assertThat(friendships.areFriends("Joe", "Audrey")).isTrue();
-        assertThat(friendships.areFriends("Audrey", "Joe")).isTrue();
-
-        assertThat(friendships.getFriendsList("Joe")).hasSize(1).contains("Audrey");
-
-        assertThat(friendships.getFriendsList("Audrey")).hasSize(1).contains("Joe");
+    public void alexDoesNotHaveFriends() {
+        assertThat(friendships.getFriendsList("Alex")).isEmpty();
     }
 
-    @Test(timeout = 500)
-    public void test3() {
-        Person joe = new Person("Joe");
-        expect(friends.findByName("Joe")).andReturn(joe).anyTimes();
+    @Test
+    public void joeHas5Friends() {
+        List<String> expected = Arrays.asList(
+                new String[]{"Audrey", "Peter", "Michael", "Britney", "Paul"}
+        );
+        Person joe = createMock(Person.class);
 
-        //doNothing().when(friends).save(any(Person.class));
+        expect(friends.findByName("Joe")).andReturn(joe);
+        expect(joe.getFriends()).andReturn(expected);
         replay(friends);
+        replay(joe);
 
-        friendships.makeFriends("Joe", "Audrey");
-        friendships.makeFriends("Joe", "Peter");
-        friendships.makeFriends("Joe", "Michael");
-        friendships.makeFriends("Joe", "Britney");
-        friendships.makeFriends("Joe", "Paul");
-
-        verify(friends);
-
-        assertThat(friendships.areFriends("Joe", "Paul")).isTrue();
-        assertThat(friendships.areFriends("Audrey", "Ralph")).isFalse();
-
-        assertThat(friendships.getFriendsList("Joe")).containsOnly("Audrey", "Peter", "Michael", "Britney", "Paul");
+        assertThat(friendships.getFriendsList("Joe"))
+                .hasSize(5)
+                .containsOnly("Audrey", "Peter", "Michael", "Britney", "Paul");
     }
+
 }
